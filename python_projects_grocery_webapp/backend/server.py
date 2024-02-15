e961466add1cbe4b30367823b6910d60943309ad
Aravind Rajesh <83217942+arav02@users.noreply.github.com>
2024-02-15 21:03:27 +0530

    Add files via upload

---
+++ python_projects_grocery_webapp/backend/server.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from sql_connection import get_sql_connection
+import mysql.connector
+import json
+import products_dao
+import orders_dao
+import uom_dao
+
+app = Flask(__name__)
+CORS(app)
+
+connection = get_sql_connection()
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    
+    if username == 'admin' and password == 'password':
+        return jsonify({'success': True}), 200
+    else:
+        return jsonify({'success': False, 'message': 'Invalid username or password'}), 401
+
+@app.route('/getUOM', methods=['GET'])
+def get_uom():
+    response = uom_dao.get_uoms(connection)
+    response = jsonify(response)
+    return response
+
+@app.route('/getProducts', methods=['GET'])
+def get_products():
+    response = products_dao.get_all_products(connection)
+    return response
+
+@app.route('/insertProduct', methods=['POST'])
+def insert_product():
+    request_payload = json.loads(request.form['data'])
+    product_id = products_dao.insert_new_product(connection, request_payload)
+    response = jsonify({
+        'product_id': product_id
+    })
+    return response
+
+@app.route('/getAllOrders', methods=['GET'])
+def get_all_orders():
+    response = orders_dao.get_all_orders(connection)
+    response = jsonify(response)
+    return response
+
+@app.route('/insertOrder', methods=['POST'])
+def insert_order():
+    request_payload = json.loads(request.form['data'])
+    order_id = orders_dao.insert_order(connection, request_payload)
+    response = jsonify({
+        'order_id': order_id
+    })
+    return response
+
+@app.route('/deleteProduct', methods=['POST'])
+def delete_product():
+    return_id = products_dao.delete_product(connection, request.form['product_id'])
+    response = jsonify({
+        'product_id': return_id
+    })
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Grocery Store Management System")
+    app.run(port=5000)
+
